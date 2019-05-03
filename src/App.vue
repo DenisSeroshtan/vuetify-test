b0398c9abf21b014f1eab2a56e5b7d294a396ef6
Denis Seroshtan <haron_777@mail.ru>
2019-05-03 23:42:44 +0500

    add drawer and toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
-    <v-toolbar app></v-toolbar>
+    <v-navigation-drawer
+      temporary
+      app
+      v-model="drawer"
+      dark
+      class="blue lighten-3"
+    >
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.title" :to="link.url">
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ link.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app dark color="light-blue accent-2">
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn :to="link.url" flat dark v-for="link in links" :key="link.title">
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
+      <router-view></router-view>
     </v-content>
     <v-footer app></v-footer>
   </v-app>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {
+          title: 'Login',
+          icon: 'lock',
+          url: '/login'
+        },
+        {
+          title: 'Registration',
+          icon: 'face',
+          url: '/registration'
+        },
+        {
+          title: 'Orders',
+          icon: 'bookmark_border',
+          url: '/orders'
+        },
+        {
+          title: 'New ad',
+          icon: 'note_add',
+          url: '/new'
+        },
+        {
+          title: 'My add',
+          icon: 'list',
+          url: '/list'
+        }
+      ]
     }
   }
 }
-</style>
+</script>
+<style lang="scss"></style>
