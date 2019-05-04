2767114197fc4f748dc520352a459c4435c3d692
DenisSeroshtan <haron_777@mail.ru>
2019-05-05 04:01:01 +0500

    add routs

--- src/router.js
+++ src/router.js
@@ -1,8 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Orders from './views/Orders.vue'
+import Registration from './views/Registration.vue'
+import AdList from './views/ads/AdList.vue'
+import AdNew from './views/ads/AdNew.vue'
+import Ad from './views/ads/Ad.vue'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      component: Home,
+      name: 'home',
+      path: '/'
+    },
+    {
+      component: Login,
+      name: 'login',
+      path: '/login'
+    },
+    {
+      component: Registration,
+      name: 'reg',
+      path: '/registration'
+    },
+    {
+      component: Orders,
+      name: 'orders',
+      path: '/orders'
+    },
+    {
+      component: AdList,
+      name: 'list',
+      path: '/list'
+    },
+    {
+      component: Ad,
+      name: 'ad',
+      path: '/ad/:id'
+    },
+    {
+      component: AdNew,
+      name: 'newAd',
+      path: '/new'
+    }
+  ],
+  mode: 'history'
 })
