f34d0259b7d1883404d40c9870515d9fda96fb89
Denis Seroshtan <haron_777@mail.ru>
2019-05-11 23:01:04 +0500

    add views orders

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,15 +1,56 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Заказы</h1>
+        <v-list subheader two-line v-for="order in orders" :key="order.id">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-checkbox
+                :input-value="order.done"
+                @change="setDone(order)"
+              ></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+                class="primary"
+                :to="{ name: 'ad', params: { id: order.idAd } }"
+                >Открыть</v-btn
+              >
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      orders: [
+        {
+          id: 'qwerty',
+          name: 'den',
+          phone: '8-999-432-23-43',
+          done: true,
+          idAd: '123'
+        }
+      ]
+    }
+  },
+  methods: {
+    setDone(order) {
+      order.done = true
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
