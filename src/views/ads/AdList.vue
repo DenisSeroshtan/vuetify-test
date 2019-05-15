e5fea31e884a22d6acac8f0fd1802fe2e399cdb1
Denis Seroshtan <haron_777@mail.ru>
2019-05-15 22:53:20 +0500

    add views adlist

--- src/views/ads/AdList.vue
+++ src/views/ads/AdList.vue
@@ -1,9 +1,63 @@
 <template>
-  <v-container>
+  <v-container grid-list-lg>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
+      <v-flex xs12 sm8 offser-sm2>
+        <h1 class="text--secondary mb-3">Список заказов</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 v-for="product in products" :key="product.id">
+        <v-card class="evalation-6 mb-3">
+          <v-layout row>
+            <v-flex xs4 class="no-padding">
+              <v-img :src="product.img" height="100%" />
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--secondary mb-3">{{ product.title }}</h2>
+                <p>{{ product.desc }}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  class="primary"
+                  :to="{ name: 'ad', params: { id: product.id } }"
+                  >Открыть</v-btn
+                >
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          title: 'title 1',
+          desc: 'some description 2',
+          promo: true,
+          img: 'https://picsum.photos/id/1/1200/400',
+          id: 12
+        },
+        {
+          title: 'title 2',
+          desc: 'some description 1',
+          promo: false,
+          img: 'https://picsum.photos/id/10/1200/400',
+          id: 123
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss">
+.no-padding {
+  padding: 0 !important;
+}
+</style>
